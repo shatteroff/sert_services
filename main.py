caaa4f5e4ebffabe212301f2cea449981f7df761
Andrew <shatter007@mail.ru>
2021-03-06 14:04:29 +0300

    --add request_email to requests

--- main.py
+++ main.py
@@ -223,5 +223,5 @@ def get_leaderboard(*args):
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0')
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0', port=8000)
